a480af09b4dc103308ca41a8db89961b776d90b4
chuchufrito <ana.jgoncalves@aluno.senac.rio>
2025-11-18 19:43:32 -0300

    inicia o repositório da aula 01

---
+++ aula01/exemplo.py
@@ -0,0 +1,70 @@
+# # # nome = 'Ana'
+# # # idade = 25
+# # # salario = 15000.0
+# # # #saida
+# # # print(f'O nome dela é {nome} \nela tem {idade} anos \ne ganha {salario} por mês')
+
+
+# # # x = 5
+# # # x = '10'
+# # # x = 100
+# # # print (x)
+
+# # # nome = input('Digite o nome: ')
+# # # print(f'Olá, {nome}')
+
+# # cor = input('Qual sua cor favorita? ')
+# # print(f'Sua cor favorita é {cor}!')
+
+# # n1 = 5
+# # n2 = 3
+# # resultado = n1 + n2
+# # print(f'O resultado foi: {resultado}')
+
+# # n1 = float(input('Digite um número de 1 à 100: '))
+# # n2 = float(input('Digite um numero de 1 à 100: '))
+# # resultado = n1 * n2
+# # print(f'O resultado foi: {resultado} ')
+
+
+# nome_usuario = input('Informe o nome: ')
+# idade_usuario = input ('Informe a idade: ')
+# print(f'Olá, {nome_usuario} de {idade_usuario} anos, BEM VINDE!!!')
+
+# cidade_usuario = input('Qual a cidade que você reside atualmente? ')
+# bairro_usuario = input('Em qual bairro fica a sua residência? ')
+# print(f'Você reside na cidade de {cidade_usuario} no bairro {bairro_usuario}')
+
+
+
+# num1 = float(input('Informe o primeiro valor: '))
+# num2 = float(input('Informe o segundo valor: '))
+# resultado = num1*num2
+# print(resultado)
+
+
+# idade_usuario = int(input('Quantos anos você tem? '))
+# ano_de_nascimento = 2025 - idade_usuario
+# print(f'Seu ano de nascimento é {ano_de_nascimento}? ')
+
+
+# caixa = float(input('Qual o valor que você tem disponível hoje? '))
+# ingresso = 15 
+# poder_de_compra = caixa//ingresso
+# print(f'Você pode comprar até {poder_de_compra} ingressos')
+
+# preco = float(input('Informe o preco: '))
+# qtd = int(input('Informe a quantidade: '))
+# total = preco*qtd 
+# desconto = total * 0.1
+# valor_final = total - desconto
+# print(f'Valor total {total:.2f}\nDesconto de 10%{desconto:.2f}\nValor a Pagar:{valor_final:.2f}')
+
+
+
+# salario= float(input('Qualo seu salário? '))
+# bonus= salario*0.15
+# total= salario+bonus
+# print(f'Esse mês você recebeu uma bonificação de 15% no seu salário, que é igual a {bonus}, totalizando {total} a receber')
+
+      
